75fa8e69b350f66560983579241a0561ad298fdf
jiagengMBP <jiageng1997@gmail.com>
2025-07-22 15:04:49 +0800

    lazy loading of sys msg once to cut down input tokens

--- src/agent.py
+++ src/agent.py
@@ -8,15 +8,16 @@ from langchain_community.tools import QuerySQLDatabaseTool
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langgraph.prebuilt import create_react_agent
-import matplotlib
 import json
+import uuid
+import matplotlib
 matplotlib.use('Agg') # non-interactive backend
 
 from dbdesc import db_description
 from prompt import latent_system_message
 
-COMMPASS_DB_URI = f'postgresql+psycopg2://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASSWORD")}@{os.environ.get("DB_HOST")}:5432/commpass'
-db = SQLDatabase.from_uri(COMMPASS_DB_URI)
+db_uri = os.environ.get("COMMPASS_DB_URI")
+db = SQLDatabase.from_uri(db_uri)
 
 REFDIR = os.environ.get("REFDIR")
 gene_annot = pd.read_csv(f'{REFDIR}/gene_annotation.tsv', sep='\t')
@@ -68,23 +69,36 @@ graph = create_react_agent(
 )
 
 # Create a system message for the agent
-# dynamic variables will be filled in later
+# dynamic variables will be filled in at the start of each session
 
-def query_agent(user_input: str, graph_url: str):
-    
-    system_message = SystemMessage(content=latent_system_message.format(
-        commpass_db_uri=COMMPASS_DB_URI,
+def create_system_message():
+    return SystemMessage(content=latent_system_message.format(
         db_description=db_description,
         dialect=db.dialect,
-        graph_url=graph_url,
+        commpass_db_uri=db_uri
     ))
 
-    user_message = HumanMessage(content=user_input)
-    
-    config = {"configurable": {"thread_id": "thread-001"}, "recursion_limit": 25}
+system_message = None
+config = {"configurable": {"thread_id": "thread-001"}, "recursion_limit": 25}
 
+def start_session():
+    global system_message
+    global graph
+    global config
+    system_message = create_system_message()
+    graph.invoke({"messages" :[system_message]}, config)
+
+def query_agent(user_input: str):
+    global system_message
+    global graph
+    global config
+    if not system_message:
+        start_session()
+    graph_png_filename = f"graph/graph_{uuid.uuid4().hex[:8]}.png"
+    preamble = SystemMessage(f'If a graph is generated, save it as {graph_png_filename} and display it with <img>.')
+    user_message = HumanMessage(content=user_input)
     full_response = ""
-    for step in graph.stream({"messages": [system_message, user_message]}, config, stream_mode="values"):
+    for step in graph.stream({"messages": [preamble, user_message]}, config, stream_mode="values"):
         if step["messages"]:
             step["messages"][-1].pretty_print()
         if step["messages"] and isinstance(step["messages"][-1], AIMessage):
