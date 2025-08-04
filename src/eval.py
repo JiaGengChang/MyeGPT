32addb93b659d0b31387c29ce554512e5c92069b
jiagengMBP <jiageng1997@gmail.com>
2025-08-04 18:02:11 +0800

    bedrock aws llm

--- src/eval.py
+++ src/eval.py
@@ -6,6 +6,7 @@ assert load_dotenv('.env')
 from langsmith import Client
 from openevals.llm import create_llm_as_judge
 from openevals.prompts import CORRECTNESS_PROMPT
+from langchain_aws import ChatBedrockConverse
 
 # Define the input and reference output pairs that you'll use to evaluate your app
 client = Client()
@@ -15,19 +16,23 @@ def target(inputs: dict) -> dict:
     response = requests.post(
         os.environ.get("APPLICATION_API_URL"),
         headers={"Content-Type": "application/json"},
-        json={"user_input": inputs["question"]}
+        json={"user_input": inputs["question"]},
     )
     answer = response.json().get("response", "No response from API")
     # Remove HTML tags
     clean_answer = re.sub(r'<.*?>', '', answer)
     return {"answer": clean_answer}
 
+llm = ChatBedrockConverse(
+    model_id=os.environ.get("MODEL_ID"),
+    temperature=0.,
+)
 
 # a correctness score from 0 to 1, where 1 is the best
 def scorer(inputs: dict, outputs: dict, reference_outputs: dict):
     evaluator = create_llm_as_judge(
         prompt=CORRECTNESS_PROMPT,
-        model="openai:o4-mini",
+        judge=llm,
         feedback_key="score",
         continuous=True,
     )
@@ -43,7 +48,7 @@ if __name__ == "__main__":
         target,
         data=os.environ.get("DATASET_NAME"),
         evaluators=[scorer],
-        max_concurrency=2,
+        max_concurrency=1,
         experiment_prefix=os.environ.get("APPLICATION_API_URL").replace("https://", "").replace("/", "_"),
         metadata={
             'model': os.environ.get("MODEL"),
