7838284edc3b60bee4a6897c7ad91238a103f21b
jiagengMBP <jiageng1997@gmail.com>
2025-08-21 16:25:08 +0800

    add missing schema field

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -2,7 +2,9 @@ import os
 from langchain_postgres import PGEngine, PGVectorStore
 from langchain_aws import BedrockEmbeddings
 
-TABLE_NAME = "vectorstore"  # do not change, set in create_vectorstore.py
+# do not change, set in create_vectorstore.py
+TABLE_NAME = "vectorstore"  
+SCHEMA_NAME = "commpass_schema"
 
 # create pgengine connection pool manager
 pg_engine = PGEngine.from_connection_string(os.environ.get("COMMPASS_DB_URI"))
@@ -16,6 +18,7 @@ def connect_store():
     store = PGVectorStore.create_sync(
         engine=pg_engine,
         table_name=TABLE_NAME,
+        schema_name=SCHEMA_NAME,
         embedding_service=embeddings,
     )
     return store
