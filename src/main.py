1ac802113f45f0bc00d0a2f8146733cdb8feaeb0
jiagengMBP <jiageng1997@gmail.com>
2025-08-29 13:08:13 +0800

    security improvements

--- src/main.py
+++ src/main.py
@@ -14,8 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET","POST"],
+    allow_headers=["Content-Type"],
 )
 
 app_dir = os.path.dirname(os.path.abspath(__file__))
